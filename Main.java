886e787e618dafde80c56c7410d0547d99bdea5e
qudej <aleksandrin2001@gmail.com>
2023-11-18 21:51:01 +0300

    debug Добавлено условие исправляющее зависание программы при равной скорости наполнения и утечки воды

--- Main.java
+++ Main.java
@@ -1,20 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         int volume = 1200;
-        int fillingSpeed = 30;
+        int fillingSpeed = 10;
         int devastationSpeed = 10;
         int startVolume = 0;
         int time = 0;
         double approximateTime;
         approximateTime = volume / fillingSpeed;
 
-        System.out.println("Примерное время наполнения  бассейна = " + approximateTime);
-        while (startVolume != volume) {
-            startVolume = startVolume + fillingSpeed - devastationSpeed;
-            time = time + 1;
-            approximateTime = (double) (volume - startVolume) / fillingSpeed;
+        if (devastationSpeed >= fillingSpeed) {
+            System.out.println("Обнаружена большая утечка в бассейне, необходимо устранить проблему или увеличить напор воды");
+        } else {
             System.out.println("Примерное время наполнения  бассейна = " + approximateTime);
+            while (startVolume != volume) {
+                startVolume = startVolume + fillingSpeed - devastationSpeed;
+                time = time + 1;
+                approximateTime = (double) (volume - startVolume) / fillingSpeed;
+                System.out.println("Примерное время наполнения  бассейна = " + approximateTime);
             }
-        System.out.println("Фактическое время наполнения бассейна = " + time);
+            System.out.println("Фактическое время наполнения бассейна = " + time);
+        }
     }
 }
